ea64ec36da0ee7fa252cfa54d307b8da26bbe7c8
Sarveswara rao <rao.sarveswara@gmail.com>
2020-07-28 12:38:00 +1000

    new component

---
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -0,0 +1,23 @@
+import { LightningElement,track } from 'lwc';
+
+export default class BmiCalculator extends LightningElement {
+    cardTitle="Good Morning Harika";
+    weight;
+    height;
+
+    @track bmi;
+
+    onWeightChange(event){
+        this.weight=parseFloat(event.target.value);
+    }
+    onHeightChange(event){
+        this.height=parseFloat(event.target.value);
+    }
+    calculateBmi(){
+        try{
+        this.bmi=this.weight/(this.height*this.height);
+        }catch(error){
+            this.bmi=undefined;
+        }
+    }
+    }
