948a1d853e8d94cd1e5fde9c61f6dcf2e4f6a89c
Sarveswara rao <rao.sarveswara@gmail.com>
2020-07-29 16:48:46 +1000

    new component

--- force-app/main/default/lwc/meetingRoom/meetingRoom.js
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -2,4 +2,6 @@ import { LightningElement,api } from 'lwc';
 
 export default class MeetingRoom extends LightningElement {
     @api meetingRoomInfo ;// {roomName : 'A-01', roomCapacity:'12'}
+
+    @api showRoomInfo=false;
 }
